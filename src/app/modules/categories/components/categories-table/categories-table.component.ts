32fb8e2c3e6ad6aaeffc4aad84162b40512ed83e
jorge.lucas <jorgegrlucas@gmail.com>
2025-05-31 21:53:04 +0100

    feat: :sparkles: Adding category component and Delete category function
    Adding category component and Delete category function

---
+++ src/app/modules/categories/components/categories-table/categories-table.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CategoryEvent } from 'src/app/models/enums/products/categories/CategoryEvent';
+import { DeleteCategoryAction } from 'src/app/models/interfaces/categories/responses/event/DeleteCategoryAction';
+import { EditCategoryAction } from 'src/app/models/interfaces/categories/responses/event/EditCategoryAction';
+import { getCategoriesResponse } from 'src/app/models/interfaces/categories/responses/getCategoriesResponse';
+
+@Component({
+  selector: 'app-categories-table',
+  templateUrl: './categories-table.component.html',
+  styleUrls: []
+})
+export class CategoriesTableComponent {
+  @Input() public categories: Array<getCategoriesResponse> = []
+  @Output() public categoryEvent = new EventEmitter<EditCategoryAction>()
+  @Output() public deleteCategoryEvent = new EventEmitter<DeleteCategoryAction>()
+
+  public categorySelected!: getCategoriesResponse
+  public addCategoryAction = CategoryEvent.ADD_CATEGORY_ACTION
+  public editCategoryAction = CategoryEvent.EDIT_CATEGORY_ACTION
+
+  handleDeleteCategoryEvent(category_id:string, categoryName: string) : void {
+    if (category_id !== '' && categoryName !== '') {
+      this.deleteCategoryEvent.emit({category_id, categoryName})
+    }
+  }
+}
---
+++ src/app/modules/categories/page/categories-home/categories-home.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+import { Subject, takeUntil } from 'rxjs';
+import { DeleteCategoryAction } from 'src/app/models/interfaces/categories/responses/event/DeleteCategoryAction';
+import { getCategoriesResponse } from 'src/app/models/interfaces/categories/responses/getCategoriesResponse';
+import { CategoriesService } from 'src/app/services/categories/categories.service';
+
+@Component({
+  selector: 'app-categories-home',
+  templateUrl: './categories-home.component.html',
+  styleUrls: [],
+})
+export class CategoriesHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public categoriesDatas: Array<getCategoriesResponse> = [];
+
+  constructor(
+    private categoriesService: CategoriesService,
+    private dialogService: DialogService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private router: Router
+  ) {}
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  getAllCategories() {
+    this.categoriesService
+      .getAllCategories()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.categoriesDatas = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar categorias!',
+            life: 3000,
+          });
+          this.router.navigate(['/dashboard']);
+        },
+      });
+  }
+
+  handleDeleteCategoryAction(event: DeleteCategoryAction): void {
+    if (event) {
+      this.confirmationService.confirm({
+        message: `Confirma a exclusão da categoria: ${event?.categoryName}`,
+        header: 'Confirmaçãod e exclusão',
+        icon: 'pi pi-exlamation-triangle',
+        acceptLabel: 'Sim',
+        rejectLabel: 'Não',
+        accept: () => this.deleteCategory(event?.category_id),
+      });
+    }
+  }
+  deleteCategory(category_id: string) {
+    if (category_id) {
+      this.categoriesService
+        .deleteCategory({ category_id })
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: 'Categoria removida com sucesso!',
+              life: 2500,
+            });
+            this.getAllCategories();
+          },
+          error: (err) => {
+            console.log(err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao remover a categoria!',
+              life: 2500,
+            });
+            this.getAllCategories();
+          },
+        });
+    }
+     this.getAllCategories();
+  }
+}
